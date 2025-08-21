12cb99cd0e045b5e0ec27860e2c4a2de3dfecc55
addya <aj11routa@yorksj.ac.uk>
2025-08-21 19:10:18 +0100

    implement basic footer

--- src/components/FooterComp.js
+++ src/components/FooterComp.js
@@ -2,10 +2,54 @@ import logo from "../images/logo-short.JPG";
 import { Link } from "react-router-dom";
 import "./components.css";
 
-import NavbarComp from "./NavbarComp.js";
-
 function FooterComp() {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer">
+      <div className="footer-directory">
+        <div className="footer-directory-column">
+          <ul className="footer-directory-category">
+            <h3>Contact Us</h3>
+            <li className="footer-list-item">Call us at 0191 942 7281</li>
+            <li className="footer-list-item">
+              Email us at help@looseleeads.co.uk
+            </li>
+            <li className="footer-list-item">
+              <a href="https://www.instagram.com/looseleadsdogwalking/?hl=en">
+                Instagram
+              </a>
+            </li>
+            <li className="footer-list-item">
+              <a href="https://www.facebook.com/people/Loose-Leads-Dog-Walking">
+                Facebook
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-directory-column">
+          <ul className="footer-directory-category">
+            <h3>Heading2</h3>
+            <li className="footer-list-item">item link 1</li>
+            <li className="footer-list-item">item link 1</li>
+          </ul>
+        </div>
+        <div className="footer-directory-column">
+          <ul className="footer-directory-category">
+            <h3>Heading3</h3>
+            <li className="footer-list-item">item link 1</li>
+            <li className="footer-list-item">item link 2</li>
+          </ul>
+        </div>
+        <div className="footer-directory-column">
+          <ul className="footer-directory-category">
+            <h3>Heading4</h3>
+            <li className="footer-list-item">item link 1</li>
+            <li className="footer-list-item">item link 2</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+  // Headings: sitemap (links to each page), contact us (phone, email, socials), social media (facebook, instagram, tiktok)
 }
 
 export default FooterComp;
